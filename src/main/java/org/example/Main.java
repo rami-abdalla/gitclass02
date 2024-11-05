6da59d47d273a2eef28ae0ed063c1e1fe43f2129
rami-abdalla <rami.abdallaa@gmail.com>
2024-11-04 23:04:39 -0500

    fourth commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,12 +5,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
+        System.out.println("This is the project version 4");
 
+        //version 4
         }
     }
